e39aeb9a6f8b033b1c212d310eb5fa56c6875682
Bobby Moynihan <bobby@gigfirm.com>
2022-06-05 05:46:18 -0500

    fixing video autoplay (inline mobile issue)

--- components/Media.vue
+++ components/Media.vue
@@ -3,7 +3,7 @@
     v-if="src.length"
     class="media"
     >
-    <video v-if="isVideo" :src="src" :controls="controls" :loop="loop" :muted="muted" :autoplay="autoplay" playsinline />
+    <video v-if="isVideo" :src="src" :controls="controls" loop muted autoplay playsinline />
     <img v-else :src="src" />
   </div>
 </template>
@@ -50,7 +50,7 @@
       }
     },
     mounted() {
-      this.playPauseWhenInView()
+      // this.playPauseWhenInView()
     },
     methods: {
       playPauseWhenInView() {
